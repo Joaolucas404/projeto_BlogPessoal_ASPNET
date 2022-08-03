79d3c71fa5a5f3bbee6c14635e50f9b13722bb3a
joaolucas404 <joaolucasssa21@gmail.com>
2022-08-03 10:52:05 -0300

    @joaolucas404[Task: commit inicial]{03/0/2022 10:51}

--- BlogAPI1/Startup.cs
+++ BlogAPI1/Startup.cs
@@ -1,16 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BlogAPI.Src.Contextos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace BlogAPI1
+namespace BlogAPI
 {
     public class Startup
     {
@@ -24,17 +26,25 @@ namespace BlogAPI1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Configuração de Bando de dados
+            services.AddDbContext<BlogPessoalContexto>(opt => opt.UseSqlServer(Configuration["ConnectionStringsDev:DefaultConnection"]));
 
+            // Controladores
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BlogPessoalContexto contexto)
         {
+            // Ambiente de desenvolvimento
             if (env.IsDevelopment())
             {
+                contexto.Database.EnsureCreated();
                 app.UseDeveloperExceptionPage();
             }
+
+            // Ambiente de produção
+            contexto.Database.EnsureCreated();
 
             app.UseRouting();
 
